27b4ff6b28045d1284ed7f98ee6793d33242f899
Wish McCarty <joannamccarty10@gmail.com>
2019-02-27 11:34:22 -0500

    add listening function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 window.addEventListener('keydown', function (e) {
-  console.log(e.keyCode);
+  // querySelector returns the first element that matches
+  // a specified css selector
+  /*attribute selector: selects elements with a specific attribute.
+    example: a[target]
+    can also select element with specific attribute and value:
+    a[target="_blank"]
+    works in both CSS and JS. */
+    /* ES6 template strings/string literals
+      Always enclosed `in backticks`
+      Written as: ${expression}*/
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  console.log(audio);
 });
+// left off at 7:17
