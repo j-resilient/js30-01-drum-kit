9a8f5757315b50f64bffce473ef2939f7ee60dad
Wish McCarty <joannamccarty10@gmail.com>
2019-02-28 11:37:56 -0500

    add css color change

--- script.js
+++ script.js
@@ -10,6 +10,35 @@ window.addEventListener('keydown', function (e) {
       Always enclosed `in backticks`
       Written as: ${expression}*/
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  // stop the function from running all together
+  if(!audio) return;
+  /* If you call play on an audio element that is already playing, it won't
+    play it again, so if you repeatedly hit a key it would only play the
+    audio one every few times. If you want it to play every time you hit
+    the key, you need to reset audio.currentTime.*/
+  audio.currentTime = 0; // rewinds sound to the start
+  audio.play();
+  /* adds the class playing to the key */
+  key.classList.add('playing');
 });
-// left off at 7:17
+
+function removeTransition(e) {
+  // if it's not a transform, skip it
+  if(e.propertyName !== 'transform') return;
+  console.log("hello");
+  this.classList.remove('playing');
+}
+
+/* Select all keys on the page and store them in keys */
+// ... except nothing is getting stored, wtf
+const keys = document.querySelectorAll('.key');
+console.log(keys.length);
+/* You can't add an eventListener to an array: you have to listen to each
+  element individually. */
+/*
+  Iterate through keys using forEach; forEach element, add an eventListener
+  to listen for the end of a transition; when that transitionEnd is heard,
+  remove the transition via the removeTransition function.
+*/
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
